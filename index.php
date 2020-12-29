94345c709e14439da4b5719d632f7bfbbe2dca11
Ivan <Ivan50592@yandex.ru>
2020-12-30 01:23:39 +0300

    Бог есть

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Кнопка выхода из безвыходных ситуаций</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+    <link  rel="stylesheet" href="style.css">
+<link rel="Stylesheet" type="text/css" href="http://devsarray.com.ar/hernan/reset.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Play:wght@700&display=swap" rel="stylesheet">
+
+<body>
+&nbsp
+<center><font size="17" class="text">КНОПКА ВЫХОДА ИЗ БЕЗВЫХОДНЫХ СИТУАЦИЙ</center><font>	
+
+
+<center><div id="red" class="button red text-blanco text-shadow-negra"></div></center>
+
+
+	</body>
+</html>
